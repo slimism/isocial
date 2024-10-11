be281b3307a5f5b902283866104daa64dc6d3c5b
Mohammad Slim <mysleeem@gmail.com>
2024-10-11 20:13:49 +0200

    added allowed hosts Commit

--- isocial/settings.py
+++ isocial/settings.py
@@ -13,8 +13,8 @@ SECRET_KEY = 'django-insecure-=f)$dd=e(0y3rojs&lj$i#1m5h3wyj=y)_k^)ric2ml8gzfsvu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['isocial-production.up.railway.app', 'https://isocial-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['isocial-production.up.railway.app', 'https://isocial-production.up.railway.app']
 
 
 # Application definition
